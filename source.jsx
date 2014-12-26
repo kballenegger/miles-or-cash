012cdb4fce939190d41374e76218fdb37cfdf45e
Kenneth Ballenegger <kenneth@ballenegger.com>
2014-12-26 22:11:40 +0100

    indent

--- source.jsx
+++ source.jsx
@@ -1,3 +1,16 @@
 var React = require('react');
 
+var App = React.createClass({
+
+  render: function render () {
+
+    return <div>
+      <h1>Should I use miles or cash?</h1>
+      <div>
+
+      </div>
+    </div>;
+  },
+});
+
 React.render(<App />, document.getElementById('app'));
